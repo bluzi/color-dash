b126a4873d0a7a9f2da30cbe3ecdf8cc6af2d52e
Eliran Pe'er <eliran013@gmail.com>
2017-07-21 21:58:23 +0300

    Remove build

--- server/build/models/userModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseUser {
-}
-exports.BaseUser = BaseUser;
-class User extends BaseUser {
-}
-exports.User = User;
--- server/src/endpoints.ts
+++
@@ -1,5 +0,0 @@
-export default [
-    'systemService',
-    'roomService',
-    'gameService',
-];
